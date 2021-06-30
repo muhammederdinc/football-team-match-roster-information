7407cef3725e629f83e0c4d9be39a0fcf665b083
Muhammed ERDİNÇ <erdincmuhammed@gmail.com>
2021-06-30 21:42:01 +0300

    store has been changed

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,13 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import home from '../modules/home/store';
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+export const storeOptions = {
   modules: {
+    home,
   },
-});
+};
+
+export default new Vuex.Store(storeOptions);
