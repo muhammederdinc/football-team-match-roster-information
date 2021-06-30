dcff0b514779cf33e19878f52960615abd62a7f0
Muhammed ERDİNÇ <erdincmuhammed@gmail.com>
2021-06-30 19:38:48 +0300

    search box has been deleted

--- src/App.vue
+++ src/App.vue
@@ -12,31 +12,13 @@ export default {
       flat
     >
       <v-container class="py-0 fill-height">
+        <v-spacer />
+
         <v-avatar
           class="mr-10"
           color="grey darken-1"
           size="32"
         />
-
-        <v-btn
-          v-for="link in links"
-          :key="link"
-          text
-        >
-          {{ link }}
-        </v-btn>
-
-        <v-spacer />
-
-        <v-responsive max-width="260">
-          <v-text-field
-            dense
-            flat
-            hide-details
-            rounded
-            solo-inverted
-          />
-        </v-responsive>
       </v-container>
     </v-app-bar>
 
