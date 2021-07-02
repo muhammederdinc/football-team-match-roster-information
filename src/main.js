ce83cf70219a6a61ffad8bb02991463431ec905a
Muhammed ERDİNÇ <erdincmuhammed@gmail.com>
2021-07-02 23:24:26 +0300

    main.scss has been added

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
+import main from './assets/styles/main.scss';
 
 Vue.config.productionTip = false;
 
@@ -10,5 +11,6 @@ new Vue({
   router,
   store,
   vuetify,
+  main,
   render: (h) => h(App),
 }).$mount('#app');
